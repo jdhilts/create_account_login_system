0151a996b0d422406b1007fd0b852c1b0d4a35db
James Hilts <jameshilts@icloud.com>
2022-04-06 18:28:41 -0700

    Added style to create account link in login page.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,12 @@
 import {Link} from '@reach/router'
-
+import '../style.css'
 
 const Login =(props)=> {
 	return(
 		<div>
 		<h1>This is the Login page.</h1>
 		<nav>
-		<Link to='/create_account'>Create Account</Link>
+		<Link className='link' to='/create_account'>Create Account</Link>
 		</nav>
 		</div>
 		)
